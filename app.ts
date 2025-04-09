67d944542c36fd8faf32dfeb5f472547a2260a21
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-09 17:55:07 -0400

    Set up basic express app

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
